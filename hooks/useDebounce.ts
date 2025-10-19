8a06dfe2596284dd471f2064db417f023e4890d1
OtaraAlex <mbechealex61@gmail.com>
2025-10-19 20:33:30 +0300

    Import useEffect in for proper effect handling

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export function useDebounce(callback: () => void, delay: number) {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
